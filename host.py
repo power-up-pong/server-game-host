75a49dde24d0bff4e4c4bdd4a3d47b103e6982d4
Jonathan Ellis <jde27@students.calvin.edu>
2021-04-02 21:06:46 -0400

    Finish first draft of game state

--- host.py
+++ host.py
@@ -10,56 +10,85 @@ QOS = 0
 USERNAME = 'cs326'  # broker username (if required)
 PASSWORD = 'piot'  # broker password (if required)
 
-PADDLE_WIDTH = 30
+PADDLE_WIDTH = 50
 MAX_PADDLE_VALUE = 1023
-X_CONSTRAINTS = [0, MAX_PADDLE_VALUE + PADDLE_WIDTH // 2]
-Y_CONSTRAINTS = [0, MAX_PADDLE_VALUE + PADDLE_WIDTH // 2]
 GAME_CYCLE = 0.1
-
-# MQTT connection callback
-
-
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        print('Connected to', BROKER)
-    else:
-        print('Connection to {} failed. Return code={}'.format(BROKER, rc))
-        os._exit(1)
+TIME_AFTER_SCORE = 1
+PADDLE_HALF = PADDLE_WIDTH // 2
+X_CONSTRAINTS = [0, MAX_PADDLE_VALUE + PADDLE_HALF]
+Y_CONSTRAINTS = [0, MAX_PADDLE_VALUE + PADDLE_HALF]
+X_MIDDLE = X_CONSTRAINTS[1] // 2
+Y_MIDDLE = Y_CONSTRAINTS[1] // 2
 
 
 class Game_State:
     def __init__(self):
-        self.paddle_pos1 = 0
-        self.paddle_pos2 = 0
-        self.ball_pos = [X_CONSTRAINTS[1] // 2, Y_CONSTRAINTS[1] // 2]
-        self.ball_Xvelocity = 3
-        self.ball_Yvelocity = 2
+        self.player1_score = 0
+        self.player2_score = 0
+        self.reset()
 
         self.client = mqtt.Client()
         self.client.username_pw_set(USERNAME, password=PASSWORD)
-        self.client.on_connect = on_connect
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        self.client.subscribe("pup/ctrl1", qos=QOS)
+        self.client.subscribe("pup/ctrl2", qos=QOS)
         self.client.connect(BROKER, PORT, 60)
+        self.client.loop_start()
 
-        try:
-            self.client.loop_start()
-        except KeyboardInterrupt:
-            self.client.loop_stop()
-            self.client.disconnect()
+    def reset(self):
+        self.publish_state()
+        self.paddle_pos1 = Y_MIDDLE
+        self.paddle_pos2 = Y_MIDDLE
+        self.ball_pos = [X_MIDDLE, Y_MIDDLE]
+        self.ball_Xvelocity = 70 * random.choice([-1, 1])
+        self.ball_Yvelocity = 50 * random.choice([-1, 1])
+        sleep(TIME_AFTER_SCORE)
+
+    def on_connect(self, client, userdata, flags, rc):
+        if rc == 0:
+            print('Connected to', BROKER)
+        else:
+            print('Connection to {} failed. Return code={}'.format(BROKER, rc))
+            os._exit(1)
+
+    def on_message(self, client, data, msg):
+        if msg.topic == "pup/ctrl1":
+            self.paddle_pos1 = msg.payload
+        elif msg.topic == "pup/ctrl2":
+            self.paddle_pos2 = msg.payload
 
     def get_state(self):
-        return '\n"paddle1": {},\n"paddle2": {},\n"ball": {}\n'.format(str(self.paddle_pos1), str(self.paddle_pos2), str(self.ball_pos))
+        return '\{\n"paddle1": {},\n"paddle2": {},\n"ball": {},\n"player1_score": {},\n"player2_score": {}\n\}'.format(
+            self.paddle_pos1, self.paddle_pos2, self.ball_pos, self.player1_score, self.player2_score)
 
-    def start(self):
-        self.ball_pos[0] += self.ball_Xvelocity
-        self.ball_pos[1] += self.ball_Yvelocity
-        if self.ball_pos[0] < X_CONSTRAINTS[0] or self.ball_pos[0] > X_CONSTRAINTS[1]:
-            self.ball_Xvelocity = -self.ball_Xvelocity
-        if self.ball_pos[1] < Y_CONSTRAINTS[0] or self.ball_pos[1] > Y_CONSTRAINTS[1]:
-            self.ball_Yvelocity = -self.ball_Yvelocity
+    def run_game_loop(self):
+        self.update_ball_pos()
 
         self.publish_state()
         sleep(GAME_CYCLE)
-        self.start()
+        self.run_game_loop()
+
+    def update_ball_pos(self):
+        self.ball_pos[0] += self.ball_Xvelocity
+        self.ball_pos[1] += self.ball_Yvelocity
+
+        if self.ball_pos[0] < X_CONSTRAINTS[0]:
+            if self.paddle_pos1 - PADDLE_HALF < self.ball_pos[1] < self.paddle_pos1 + PADDLE_HALF:
+                self.ball_Xvelocity = -self.ball_Xvelocity
+            else:
+                self.player2_score += 1
+                self.reset()
+
+        elif self.ball_pos[0] > X_CONSTRAINTS[1]:
+            if self.paddle_pos2 - PADDLE_HALF < self.ball_pos[1] < self.paddle_pos2 + PADDLE_HALF:
+                self.ball_Xvelocity = -self.ball_Xvelocity
+            else:
+                self.player1_score += 1
+                self.reset()
+
+        if self.ball_pos[1] < Y_CONSTRAINTS[0] or self.ball_pos[1] > Y_CONSTRAINTS[1]:
+            self.ball_Yvelocity = -self.ball_Yvelocity
 
     def publish_state(self):
         (result, num) = self.client.publish(
@@ -67,6 +96,14 @@ class Game_State:
         if result != 0:
             print('PUBLISH returned error:', result)
 
+    def get_client(self):
+        return self.client
+
 
 gs = Game_State()
-gs.start()
+try:
+    gs.run_game_loop()
+except KeyboardInterrupt:
+    client = gs.get_client()
+    client.loop_stop()
+    client.disconnect()
